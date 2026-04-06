bf7dcb60e67eca2c1daaec19a01aca0b0e415b36
Jitendra Maharana <deepakmaharana3500@gmail.com>
2026-04-06 23:23:46 +0530

    error fix

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const AcceptTask = ({data}) => {
+const AcceptTask = ({ data }) => {
+  console.log(data)
   return (
     <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
               <div className='flex justify-between items-center '>
